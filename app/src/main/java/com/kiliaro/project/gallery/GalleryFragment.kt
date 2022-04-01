7d362d0bb2113caf7cc9c730be855579c2c5b368
amirhosein <amirho3ein.rostami@yahoo.com>
2022-04-01 22:47:59 +0430

    Add navigation component

--- app/src/main/java/com/kiliaro/project/gallery/GalleryFragment.kt
+++ app/src/main/java/com/kiliaro/project/gallery/GalleryFragment.kt
@@ -1,8 +1,9 @@
 package com.kiliaro.project.gallery
 
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.kiliaro.project.R
 
 class GalleryFragment : Fragment(R.layout.fragment_gallery) {
-
+    val viewModel: GalleryViewModel by viewModels()
 }
